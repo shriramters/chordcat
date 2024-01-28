8e2b2351e6a99190f01ddef2059f2dcaf04b7a9d
Shriram Ravindranathan <127589779+shriramters@users.noreply.github.com>
2024-01-28 20:55:11 +0530

    Add icon

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,12 @@
 // SPDX-License-Identifier: GPL-3.0-only
 #include <SFML/Graphics.hpp>
 #include "main_screen.hpp"
+#include "appicon.c"
 
 int main()
 {
     auto window = sf::RenderWindow{ { 1000u, 800u }, "chordcat" };
+    window.setIcon(chordcat_icon.width, chordcat_icon.height, chordcat_icon.pixel_data);
     window.setFramerateLimit(144);
 
     std::shared_ptr<AppState> state = std::static_pointer_cast<AppState>(std::make_shared<MainScreen>(window));
